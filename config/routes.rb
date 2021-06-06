67dc201ae5171a766e9de105129f7bcb3b15e6c1
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-06-05 23:05:33 -0400

    commit tree 923419017cebaffd2fea0b05470fc069f64c5591

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :coupons
+  get '/coupon/:id', to: 'coupons#show', as: 'user' # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
